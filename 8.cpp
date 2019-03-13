7c3d0e1a83c934a0e115fbcfeff38feee797f066
Goushiikh <47105396+Goushiikh@users.noreply.github.com>
2019-03-13 15:45:26 +0530

    Update 8.cpp

--- 8.cpp
+++ 8.cpp
@@ -1,31 +1,27 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-  int num1, num2, i, num, digit, sum;
-
+  int n1,n2,i,n,digit,sum;
   cout << "Enter first number: ";
-  cin >> num1;
-
+  cin >>n1;
   cout << "Enter second number: ";
-  cin >> num2;
-
-  cout << "Armstrong numbers between " << num1 << " and " << num2 << " are: " << endl;
-  for(i = num1; i <= num2; i++)
+  cin >>n2;
+  cout << "Armstrong numbers between " << n1 << " and " << n2 << " are: " << endl;
+  for(i = n1; i<= n2; i++)
   {
         sum = 0;
-        num = i;
+        n = i;
 
-        for(; num > 0; num /= 10)
+        for(n > 0; n /= 10)
         {
-            digit = num % 10;
-            sum = sum + digit * digit * digit;
+            digit = n % 10;
+            sum = sum + digit*digit*digit;
         }
 
-        if(sum == i)
+        if(sum==i)
         {
-            cout << i << endl;
+            cout <<i<<endl;
         }
   }
 
